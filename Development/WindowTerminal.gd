acc62b3df69fcf07a6d88feed920de3e6a331294
Uriel Deveaud <uriel.deveaud@gmail.com>
2019-10-02 10:23:35 -0500

    simple update

--- Development/WindowTerminal.gd
+++ Development/WindowTerminal.gd
@@ -1,20 +1,16 @@
-extends PopupPanel
+extends WindowDialog
 
 # Declare member variables here. Examples:
-# var a: int = 2
-# var b: String = "text"
+# var a = 2
+# var b = "text"
 
 # Called when the node enters the scene tree for the first time.
-func _ready() -> void:
+func _ready():
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta: float) -> void:
+#func _process(delta):
 #	pass
-
-
-func _on_Button_Window_pressed() -> void:
-	visible = !visible
 
 
 func _on_ButtonTerminal_pressed():
